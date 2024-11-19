2de1befcb9daf64c8bc3570e58e6ae9d359fcf6c
aurnabdas <aurnabdas693@gmail.com>
2024-11-19 02:56:02 -0500

    Added More API calls, and gemini implemenation

---
+++ Film-Lookup/Views/APIKey.swift
@@ -0,0 +1,8 @@
+//
+//  APIKey.swift
+//  Film-Lookup
+//
+//  Created by Aurnab Das on 11/17/24.
+//
+
+import Foundation
---
+++ Film-Lookup/Views/Api/WatchModeApi.swift
@@ -0,0 +1,8 @@
+//
+//  WatchModeApi.swift
+//  Film-Lookup
+//
+//  Created by Aurnab Das on 11/11/24.
+//
+
+import Foundation
