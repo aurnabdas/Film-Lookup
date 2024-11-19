2b63cc51fbd5c07f19678b79310655a92fa457df
aurnabdas <83616888+aurnabdas@users.noreply.github.com>
2024-11-19 03:09:02 -0500

    Update NowPlayingAPI.swift

--- Film-Lookup/Views/Api/NowPlayingAPI.swift
+++ Film-Lookup/Views/Api/NowPlayingAPI.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+
+struct NowPlayingMovie: Decodable {
+    let posterPath: String?
+
+    enum CodingKeys: String, CodingKey {
+        case posterPath = "poster_path"
+    }
+}
+
+
+struct NowPlayingResponse: Decodable {
+    let results: [NowPlayingMovie]
+}
+
+
+
+
+func fetchNowPlayingPosters() async throws -> [String] {
+
+    guard let nowPlayKey = getAPIKey() else {
+            throw NSError(domain: "APIKeyError", code: 0, userInfo: [NSLocalizedDescriptionKey: "TMDB API key not found"])
+        }
+    let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing")!
+    var components = URLComponents(url: url, resolvingAgainstBaseURL: true)!
+    let queryItems: [URLQueryItem] = [
+        URLQueryItem(name: "language", value: "en-US"),
+        URLQueryItem(name: "page", value: "1"),
+    ]
+    components.queryItems = queryItems
+
+    var request = URLRequest(url: components.url!)
+    request.httpMethod = "GET"
+    request.timeoutInterval = 10
+    request.allHTTPHeaderFields = [
+        "accept": "application/json",
+        "Authorization": "Bearer \(nowPlayKey)"
+    ]
+
+    let (data, _) = try await URLSession.shared.data(for: request)
+
+
+    let response = try JSONDecoder().decode(NowPlayingResponse.self, from: data)
+
+    // first 10 posters are taken
+    return response.results.prefix(10).compactMap { $0.posterPath }
+}
+
+
